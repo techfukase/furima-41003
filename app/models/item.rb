6866fdc9a46d79beea483ff9e4a0d93d61f0d462
techfukase <techfukase1451102@gmail.com>
2024-07-02 18:05:30 +0900

    Active hash関連の記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :items_status
+  belongs_to :delivery_price
+  belongs_to :prefecture
+  belongs_to :delivery_date  
 end
