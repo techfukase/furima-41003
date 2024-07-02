b22bbc9595da329cf5e7e2acb8fe511670031aa8
techfukase <techfukase1451102@gmail.com>
2024-07-02 16:44:09 +0900

    ActiveHash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,16 @@ class ItemsController < ApplicationController
   def new
     @items = Items.new
   end
-    
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to root_path
+  end
+  
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :content, :category_id, :items_status_id, :delivery_price_id, :prefecture_id, :delivery_date_id, :items_price, :image).merge(user_id: current_user.id)
+  end  
 end
