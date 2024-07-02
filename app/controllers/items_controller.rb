af264032b40b595850519c0022965e2398bd0be2
techfukase <techfukase1451102@gmail.com>
2024-07-02 18:32:00 +0900

    ビューファイル編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
   def index
   end
 
   def new
-    @items = Items.new
+    @item = Item.new
   end
 
   def create
-    @item = Item.create(item_params)
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   
@@ -16,5 +21,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :content, :category_id, :items_status_id, :delivery_price_id, :prefecture_id, :delivery_date_id, :items_price, :image).merge(user_id: current_user.id)
-  end  
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_02_081118) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_02_083615) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_02_081118) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "delivery_dates", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "delivery_prices", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,6 +60,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_02_081118) do
   end
 
   create_table "items_statuses", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -79,5 +89,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_02_081118) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "users"
 end
