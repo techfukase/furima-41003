04be886359adc58f6a2cb3340e33e6cb016c47e3
techfukase <techfukase1451102@gmail.com>
2024-07-02 12:26:57 +0900

    itemモデル

---
+++ db/migrate/20240702025109_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string     :title, null: false
+      t.text       :content, null: false
+      t.integer    :category_id, null: false
+      t.integer    :items_status_id, null: false
+      t.integer    :delivery_price_id, null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :delivery_date_id, null: false
+      t.integer    :items_price_id, null: false
+      t.references :user, null: false, foreign_key: true      
+      t.timestamps
+    end
+  end
+end
