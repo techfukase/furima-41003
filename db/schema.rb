467fc43b911f6ab24dc17d3a61f314882256b63e
techfukase <techfukase1451102@gmail.com>
2024-07-03 15:54:43 +0900

    バリデーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_03_063425) do
     t.bigint "user_id", null: false   
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "items_price"
+    t.integer "items_price", null: false
     t.index ["user_id"], name: "index_items_on_user_id"    
   end
 
