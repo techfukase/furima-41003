4ae91763b97a62e539851912398ae9cd9a2d8f97
techfukase <techfukase1451102@gmail.com>
2024-07-04 21:11:14 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update]
-  before_action :moved_top_page, only: [:edit, :destroy]
+  before_action :moved_top_page, only: :edit
 
   def index
     @items = Item.order(created_at: :DESC)
