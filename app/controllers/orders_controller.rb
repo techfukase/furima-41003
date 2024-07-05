19150b8291b332d5cbaed8fc79bc225faf3d99a5
techfukase <techfukase1451102@gmail.com>
2024-07-05 15:45:53 +0900

    コントローラー、ビュー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :item_find, only: :index
+
   def index
     @order = Order.new
     @address = Address.new
   end
+
+  private
+
+  def item_find
+    @item = Item.find(params[:item_id])
+  end  
 end
