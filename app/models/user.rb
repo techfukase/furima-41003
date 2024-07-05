59ad163af3380393ed4e8e5af0565716a6ac3999
techfukase <techfukase1451102@gmail.com>
2024-07-05 12:11:32 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-
+  has_many :orders
+  
   validates :nickname, presence: true
   validates :birthday, presence: true
 
