f58ea0bb43257a2e38764ca8527eefe982fcb1e4
techfukase <techfukase1451102@gmail.com>
2024-07-06 18:47:54 +0900

    order_addressモデル作成とバリデーション設定

---
+++ app/models/order_address.rb
@@ -0,0 +1,23 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :number, :building, :phone_number, :order_id
+
+  VALID_POST_NUMBER_REGEX = /\A\d{3}-\d{4}\z/
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+
+    validates :post_number, 
+              format: { with: VALID_POST_NUMBER_REGEX, message: 'は「3桁ハイフン4桁」の形式で入力してください' }
+
+    validates :prefecture_id,
+              numericality: { other_than: 1, message: 'を選択してください' }
+
+    validates :city
+    validates :number
+
+    validates :phone_number,
+              format: { with: /\A\d{10,11}\z/, message: 'はハイフンなしの半角数値で入力してください' }
+  end  
+end
