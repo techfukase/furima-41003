772ca5dc67402ba655cff638dc284caf38af30d0
techfukase <techfukase1451102@gmail.com>
2024-07-06 18:06:32 +0900

    orders controller修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,11 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-    Address.create(address_params)
-    if @order.valid?
-      @order.save
+    @item = Item.find(params[:item_id])
+    @order = Order.create(order_params)
+    @address = Address.create(address_params)
+
+    if @order.save && @address.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
@@ -19,6 +20,10 @@ class OrdersController < ApplicationController
 
   private
 
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+  
   def address_params
     params.permit(:post_number, :prefecture_id, :city, :number, :building, :phone_number).merge(order_id: @order.id)
   end
