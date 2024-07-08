8402fc1cf18b1965088fbd088a81f6b8e23b61d8
techfukase <techfukase1451102@gmail.com>
2024-07-08 10:46:17 +0900

    保存処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -19,5 +19,11 @@ class OrderAddress
 
     validates :phone_number,
               format: { with: /\A\d{10,11}\z/, message: 'はハイフンなしの半角数値で入力してください' }
-  end  
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, number: number,
+                   building: building, phone_number: phone_number, order_id: order_id)
+  end
 end
