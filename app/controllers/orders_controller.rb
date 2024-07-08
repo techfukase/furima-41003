bc82c9524b84a962653f2d5dffabe00e52bdbb1a
techfukase <techfukase1451102@gmail.com>
2024-07-08 11:16:56 +0900

    フォームオブジェクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,15 @@ class OrdersController < ApplicationController
   before_action :item_find, only: :index
 
   def index
-    @order = Order.new
-    @address = Address.new
+    @order_address = OrderAddress.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = Order.create(order_params)
-    @address = Address.create(address_params)
+    @order_address = OrderAddress.new(order_params)
 
-    if @order.save && @address.save
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
@@ -21,10 +20,8 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_number, :prefecture_id, :city, :number, :building,
+                                          :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
   
-  def address_params
-    params.permit(:post_number, :prefecture_id, :city, :number, :building, :phone_number).merge(order_id: @order.id)
-  end
 end
